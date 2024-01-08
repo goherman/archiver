62e48b07f42e2b010d0c909eff432258d195fc3b
goherman <fs0c1ety@duck.com>
2024-01-08 23:56:30 +0300

    one added, one modified

--- lib/vlc/vlc_test.go
+++ lib/vlc/vlc_test.go
@@ -68,34 +68,6 @@ func Test_encodeBin(t *testing.T) {
 	}
 }
 
-func Test_splitByChunks(t *testing.T) {
-	type args struct {
-		bStr      string
-		chunkSize int
-	}
-	tests := []struct {
-		name string
-		args args
-		want BinaryChunks
-	}{
-		{
-			name: "Base name",
-			args: args{
-				bStr:      "001000100110100101",
-				chunkSize: 8,
-			},
-			want: BinaryChunks{"00100010", "01101001", "01000000"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := splitByChunks(tt.args.bStr, tt.args.chunkSize); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("splitByChunks() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestBinaryChunks_ToHex(t *testing.T) {
 	tests := []struct {
 		name string
